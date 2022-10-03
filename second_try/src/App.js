e51daafe13e42b1c39b9f2cd013c3e8921fefc8d
AmirWorkplace <amirralli300400@gmail.com>
2022-10-03 09:04:25 +0600

    I want to create a POP UP menu

--- second_try/src/App.js
+++ second_try/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./App.css";
-import Clickcounter from "./components/ClickCounter";
-import Clickcounters from "./components/Clickcounters";
-import HoverCounter from "./components/HoverCounter";
 
 function App() {
   return (
     <div className="App">
-      <Clickcounter />
-      <Clickcounters />
-      <HoverCounter />
+      <h1>Syed Amir Ali</h1>
     </div>
   );
 }
